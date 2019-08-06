7a3324f58b274e82402ee4271c84b45e9c48dbc5
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2019-08-06 14:13:39 +0200

    scripts/serato_markers2.py: Add support for FLIP entries

--- scripts/serato_markers2.py
+++ scripts/serato_markers2.py
@@ -123,9 +123,43 @@ class LoopEntry(Entry):
         ))
 
 
+class FlipEntry(Entry):
+    NAME = 'FLIP'
+    FMT1 = 'cB?'
+    FMT2 = '>?I'
+    FMT3 = '>BI16s'
+    FIELDS = ('field1', 'index', 'enabled', 'name', 'loop', 'num_actions',
+              'actions')
+
+    @classmethod
+    def load(cls, data):
+        info1_size = struct.calcsize(cls.FMT1)
+        info1 = struct.unpack(cls.FMT1, data[:info1_size])
+        name, nullbyte, other = data[info1_size:].partition(b'\x00')
+        assert nullbyte == b'\x00'
+
+        info2_size = struct.calcsize(cls.FMT2)
+        loop, num_actions = struct.unpack(cls.FMT2, other[:info2_size])
+        action_data = other[info2_size:]
+        actions = []
+        for i in range(num_actions):
+            type_id, size = struct.unpack(cls.FMT2, action_data[:info2_size])
+            action_data = action_data[info2_size:]
+            assert type_id == 0
+            payload = struct.unpack('>dd', action_data[:size])
+            action_data = action_data[size:]
+            actions.append(payload)
+        assert action_data == b''
+
+        return cls(*info1, name.decode('utf-8'), loop, num_actions, actions)
+
+    def dump(self):
+        raise NotImplementedError('FLIP entry dumps are not implemented!')
+
+
 def get_entry_type(entry_name):
     entry_type = UnknownEntry
-    for entry_cls in (BpmLockEntry, ColorEntry, CueEntry, LoopEntry):
+    for entry_cls in (BpmLockEntry, ColorEntry, CueEntry, LoopEntry, FlipEntry):
         if entry_cls.NAME == entry_name:
             entry_type = entry_cls
             break
