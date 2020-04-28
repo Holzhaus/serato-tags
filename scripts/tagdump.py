8b9105f94e0879f97314e6b11b717057be6ec7af
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-04-28 21:06:23 +0200

    scripts/tagdump: Allow dumping encoded data

--- scripts/tagdump.py
+++ scripts/tagdump.py
@@ -6,7 +6,7 @@ import os.path
 import mutagen
 
 
-def get_serato_tagdata(tagfile):
+def get_serato_tagdata(tagfile, decode=False):
     if tagfile and tagfile.tags:
         if isinstance(tagfile, (mutagen.mp3.MP3, mutagen.aiff.AIFF)):
             for tagname, tagvalue in tagfile.tags.items():
@@ -23,20 +23,22 @@ def get_serato_tagdata(tagfile):
                 else:
                     encoded_data = bytes(tagvalue[0])
 
-                length = len(encoded_data.splitlines()[-1])
+                fixed_data = encoded_data
+
+                length = len(fixed_data.splitlines()[-1])
                 if length % 4 == 1:
-                    encoded_data += b'A=='
+                    fixed_data += b'A=='
                 elif length % 4 == 2:
-                    encoded_data += b'=='
+                    fixed_data += b'=='
                 elif length % 4 == 3:
-                    encoded_data += b'='
-                data = base64.b64decode(encoded_data)
+                    fixed_data += b'='
+                data = base64.b64decode(fixed_data)
 
                 assert data.startswith(b'application/octet-stream\0')
                 fieldname_endpos = data.index(b'\0', 26)
                 fieldname = data[26:fieldname_endpos].decode()
                 fielddata = data[fieldname_endpos + 1:]
-                yield fieldname, fielddata
+                yield fieldname, fielddata if decode else encoded_data
         elif isinstance(tagfile, mutagen.oggvorbis.OggVorbis):
             for tagname, tagvalue in tagfile.tags.items():
                 if not tagname.startswith('serato_'):
@@ -48,10 +50,11 @@ def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('output_dir')
     parser.add_argument('input_file')
+    parser.add_argument('-d', '--decode', action='store_true')
     args = parser.parse_args(argv)
 
     tagfile = mutagen.File(args.input_file)
-    for field, value in get_serato_tagdata(tagfile):
+    for field, value in get_serato_tagdata(tagfile, decode=args.decode):
         filename = '{name}.octet-stream'.format(name=field)
         filepath = os.path.join(args.output_dir, filename)
         with open(filepath, mode='wb') as fp:
